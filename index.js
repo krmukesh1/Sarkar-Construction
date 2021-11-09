ddf5b486fe22e6dd7d3d51567d92e639ebfbe839
email <krmukeshsingh51@gmail.com>
2021-11-10 02:01:13 +0530

    added codes of localStorage

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ const darkMode = () => {
 };
 
 icon.addEventListener("click", () => {
+  // Get the value of the "dark" item from the local storage on every click
   setDarkMode = localStorage.getItem("dark-themee");
   if (setDarkMode !== "on") {
     darkMode();
+    // set the value of the item to "on " when dark ode is on
     setDarkMode = localStorage.setItem("dark-themee", "on");
   } else {
     darkMode();
+    // set the value of the item to "null " when dark ode is off
     setDarkMode = localStorage.setItem("dark-themee", null);
   }
 });
 
-// localStorage
+// get the value of the "dark-themee" item from the local Storage
 let setDarkMode = localStorage.getItem("dark-themee");
+// check dark mode is on or off on Page realod
 if (setDarkMode === "on") {
   darkMode();
 }
